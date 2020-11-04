a8c08c9bfbcd00039567720d06c2aa94355640cc
vivianmarcello-cpfl <71657878+vivianmarcello-cpfl@users.noreply.github.com>
2020-11-04 15:46:48 -0300

    Create notebook_executor_2.sh

---
+++ notebook_executor_2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+if lspci -vnn | grep NVIDIA > /dev/null 2>&1; then
+  # Nvidia card found, need to check if driver is up
+  if ! nvidia-smi > /dev/null 2>&1; then
+    echo "Installing driver"
+    /opt/deeplearning/install-driver.sh
+  fi
+fi
+
+if [[ ! -z $(command -v conda) ]]; then
+  pip install -U papermill>=1.0.1
+fi
+
+pip install pandasql
+pip install curl 
+
+readonly INPUT_NOTEBOOK_GCS_FILE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/input_notebook -H "Metadata-Flavor: Google")
+readonly OUTPUT_NOTEBOOK_GCS_FOLDER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/output_notebook -H "Metadata-Flavor: Google")
+readonly PARAMETERS_GCS_FILE=$(curl --fail http://metadata.google.internal/computeMetadata/v1/instance/attributes/parameters_file -H "Metadata-Flavor: Google")
+
+readonly TEMPORARY_NOTEBOOK_FOLDER="/tmp/notebook"
+mkdir "${TEMPORARY_NOTEBOOK_FOLDER}"
+
+readonly OUTPUT_NOTEBOOK_NAME=$(basename ${INPUT_NOTEBOOK_GCS_FILE})
+readonly OUTPUT_NOTEBOOK_CLEAN_NAME="${OUTPUT_NOTEBOOK_NAME%.ipynb}-clean"
+readonly TEMPORARY_NOTEBOOK_PATH="${OUTPUT_NOTEBOOK_GCS_FOLDER}/${OUTPUT_NOTEBOOK_NAME}"
+# For backward compitability.
+readonly LEGACY_NOTEBOOK_PATH="${TEMPORARY_NOTEBOOK_FOLDER}/notebook.ipynb"
+
+PAPERMILL_EXIT_CODE=0
+if [[ -z "${PARAMETERS_GCS_FILE}" ]]; then
+  echo "No input parameters present"
+  papermill "${INPUT_NOTEBOOK_GCS_FILE}" "${TEMPORARY_NOTEBOOK_PATH}" --log-output || PAPERMILL_EXIT_CODE=1
+else
+  echo "input parameters present"
+  echo "GCS file with parameters: ${PARAMETERS_GCS_FILE}"
+  gsutil cp "${PARAMETERS_GCS_FILE}" params.yaml
+  papermill "${INPUT_NOTEBOOK_GCS_FILE}" "${TEMPORARY_NOTEBOOK_PATH}" -f params.yaml --log-output || PAPERMILL_EXIT_CODE=1
+fi
+PAPERMILL_RESULTS=$?
+
+
+echo "Papermill exit code is: ${PAPERMILL_EXIT_CODE}"
+
+
+file="/tmp/test.txt"
+
+
+if [[ "${PAPERMILL_EXIT_CODE}" -ne 0 ]]; then
+  echo "Unable to execute notebook. Exit code: ${PAPERMILL_EXIT_CODE}"
+  file = "${TEMPORARY_NOTEBOOK_FOLDER}/FAILED.txt" 
+  echo "${PAPERMILL_RESULTS}" > ${file}
+  echo "${PAPERMILL_EXIT_CODE}" >> ${file}
+  cat  ${file}
+  # For backward compitability.
+  cp "${TEMPORARY_NOTEBOOK_PATH}" "${LEGACY_NOTEBOOK_PATH}"
+  gsutil rsync -r "${TEMPORARY_NOTEBOOK_FOLDER}" "${OUTPUT_NOTEBOOK_GCS_FOLDER}"
+fi
+
+readonly INSTANCE_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/name -H "Metadata-Flavor: Google")
+INSTANCE_ZONE="/"$(curl http://metadata.google.internal/computeMetadata/v1/instance/zone -H "Metadata-Flavor: Google")
+INSTANCE_ZONE="${INSTANCE_ZONE##/*/}"
+readonly INSTANCE_PROJECT_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/project/project-id -H "Metadata-Flavor: Google")
